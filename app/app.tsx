6914dd1e51cb1d760aa87151c5436fa42c47a5b7
yusuke122 <youfufumu@gmail.com>
2023-11-10 20:23:43 +0900

    return first add

---
+++ app/app.tsx
@@ -0,0 +1,14 @@
+//"use client"//とかくとクライアントコンポーネントになる
+//デフォルトはサーバーコンポーネント
+
+//app配下のディレクトリはルーティング対象
+import React from "react"
+import {HPPages} from "../components/HPPages"
+
+export default function app()
+{
+  return(
+    <>
+    </>
+  )
+}
